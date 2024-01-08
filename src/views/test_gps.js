3d971e6c16954ff317ece10ec1df8ead9a5bc97d
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2024-01-08 13:32:18 +0700

    up project

---
+++ src/views/test_gps.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GPSTracker from '../components/GPSTracker';
+
+function TestGPS() {
+  return (
+    <div className="TestGPS">
+      <h1>ติดตาม GPS และระยะทางที่วิ่ง</h1>
+      <GPSTracker />
+    </div>
+  );
+}
+
+export default TestGPS;
