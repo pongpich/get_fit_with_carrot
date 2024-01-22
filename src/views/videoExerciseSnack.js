efad570ecf7fbc2cba1a719181abc68d812dd997
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2024-01-22 14:37:26 +0700

    ใส่ modal

--- src/views/videoExerciseSnack.js
+++ src/views/videoExerciseSnack.js
@@ -152,7 +152,9 @@ const VideoExerciseSnack = () => {
     <>
       <div className="">
         <div className="trailer" id={`popupVDOSnack`}>
-          <div> {/*  <VideoPlayerSnack url={url} videoId={videoId} /> */}</div>
+          <div>
+            <VideoPlayerSnack url={url} videoId={videoId} />
+          </div>
         </div>
 
         <table className="table table-responsive">
@@ -314,7 +316,11 @@ const VideoExerciseSnack = () => {
                             />
                             <span className="text-random">สุ่มคลิปใหม่</span>
                           </div>
-                          <div className="box-random">
+                          <div
+                            className="box-random"
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                          >
                             <img
                               src="../assets/img/renew.png"
                               width={24}
@@ -330,6 +336,52 @@ const VideoExerciseSnack = () => {
               })}
           </tbody>
         </table>
+      </div>
+
+      {/*    <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button> */}
+
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Modal title
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
